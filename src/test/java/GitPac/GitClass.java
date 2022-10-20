555aead5b4b1ddde6b365cd801fdc2ed11aeb16f
Harishanaga <nagaharisha99@gmail.com>
2022-10-20 10:57:20 +0530

    commitimng

---
+++ src/test/java/GitPac/GitClass.java
@@ -0,0 +1,20 @@
+package GitPac;
+
+public class GitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=101;
+		int sum=0;int rem=0;
+		int temp=num;
+		while(num>0)
+		{
+			rem=num%10;
+			sum=sum+rem;
+			num=num/10;
+			
+		}
+		
+	}
+
+}
